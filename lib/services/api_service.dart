0b9624961842bb203bc88adabb79078aeaf92cfe
koladypl <133044856+koladypl@users.noreply.github.com>
2025-01-27 14:03:27 +0100

    Update api_service.dart

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -4,7 +4,7 @@ import 'package:job_seeker/job_offer.dart';
 
 
 class ApiService {
-  static const String _baseUrl = 'https://www.arbeitnow.com/api/job-board-api'; // URL API
+  static const String _baseUrl = 'https://www.arbeitnow.com/api/job-board-api'; //link
 
   static Future<List<JobOffer>> fetchJobOffers() async {
     final response = await http.get(Uri.parse(_baseUrl));
@@ -12,10 +12,9 @@ class ApiService {
     if (response.statusCode == 200) {
       final Map<String, dynamic> jsonData = json.decode(response.body);
 
-      // Upewniamy się, że `data` to lista
+      // lista
       final List<dynamic> jobData = jsonData['data'];
 
-      // Konwertujemy listę na obiekty JobOffer
       return jobData.map((json) => JobOffer.fromJson(json)).toList();
     } else {
       throw Exception('Failed to load job offers');
